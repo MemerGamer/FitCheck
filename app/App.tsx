326c0b980c9cda6d58a3396d013723f05c530fc6
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2024-05-13 17:41:36 +0300

    feat(app): created initial app

---
+++ app/App.tsx
@@ -0,0 +1,12 @@
+import "./global.css"
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl text-center p-4">Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
