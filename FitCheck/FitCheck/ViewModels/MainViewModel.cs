f0bc1553355ffda43044dc6a07a0d225d7818f1f
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2024-05-13 12:22:38 +0300

    refactor: removed avalonia starter

--- FitCheck/FitCheck/ViewModels/MainViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace FitCheck.ViewModels;
-
-public class MainViewModel : ViewModelBase
-{
-    public string Greeting => "Welcome to Avalonia!";
-}
--- FitCheck/FitCheck/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using ReactiveUI;
-
-namespace FitCheck.ViewModels;
-
-public class ViewModelBase : ReactiveObject
-{
-}
